6eeb6bc2eb4a4f981b1ca789ece01e3180dcd055
Tyler Manning <tylermanning@fastmail.com>
2020-05-31 20:49:45 -0400

    Final

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Spreact
-Python script to generate a Spring Boot + ReactJS project the easy way.
+
+Easy to use scipt-utility for creating a Spring Backend + ReactJS Frontend Application
+
+## Steps to Generate Project
+
+1. Clone repo
+2. Run `pip install requirements.txt`
+3. Run `python spreact.py`
+4. Fill out Project Details
+5. Done!
--- spreact.py
+++ spreact.py
@@ -3,6 +3,7 @@ import sys
 import json
 import subprocess
 import os
+import zipfile
 
 from consolemenu import SelectionMenu
 
@@ -11,18 +12,29 @@ languages = ["Java", "Kotlin", "Groovy"]
 jvm_versions = ["8", "11", "14"]
 
 
+def getSelectionResult(list, title, subtitle=None):
+    var = SelectionMenu.get_selection(
+        list, title=title, subtitle=subtitle, show_exit_option=False)
+    return list[var]
+
+
 class Project():
 
     name_of_project = input("Name of Project: ")
-    # artifact_group = input("Artifact group (ex: org.tylermanning): ")
-    # description = input("Description: ")
-    # dependency_system = getSelectionResult(
-    #     java_depedency_systems, "Select Java Dependency Manager: ")
-    # language = getSelectionResult(
-    #     languages, "Select Backend Language Target: ")
+
+    artifact_input = input("Artifact group (ex: com.example): ")
+    artifact_group = "com.example" if artifact_input == "" else artifact_input
+
+    description = input("Description: ")
+    dependency_system = getSelectionResult(
+        java_depedency_systems, "Select Java Dependency Manager: ")
+    language = getSelectionResult(
+        languages, "Select Backend Language Target: ")
+    use_typescript = True if input(
+        "Use typescript? (y/n): ").lower() == "y" else False
 
     def __init__(self):
-        # self.generateSpringProject()
+        self.generateSpringProject()
         self.generateReactProject()
 
     def generateSpringProject(self):
@@ -42,19 +54,32 @@ class Project():
         )
         r = requests.get(
             'https://start.spring.io/starter.zip', params=params)
-        with open("proj.zip", 'wb') as fd:
+
+        zip_file_path = f"{self.name_of_project}-api.zip"
+        with open(zip_file_path, 'wb') as fd:
             for chunk in r.iter_content(chunk_size=128):
                 fd.write(chunk)
 
+        directory_path = f"{self.name_of_project}-api"
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+            os.mkdir(directory_path)
+            zip_ref.extractall(directory_path)
+            os.remove(zip_file_path)
+
     def generateReactProject(self):
-        subprocess.call(["npx", "create-react-app",
-                         self.name_of_project.lower()], shell=True)
-
-
-def getSelectionResult(list, title, subtitle=None):
-    var = SelectionMenu.get_selection(
-        list, title=title, subtitle=subtitle, show_exit_option=False)
-    return list[var]
+        if (self.use_typescript):
+            subprocess.call(["npx", "create-react-app",
+                             self.name_of_project.lower(), "--template", "typescript"], shell=True)
+        else:
+            subprocess.call(["npx", "create-react-app",
+                             self.name_of_project.lower()], shell=True)
+        os.chdir(self.name_of_project.lower())
+        with open("package.json", "r+") as jsonFile:
+            data = json.load(jsonFile)
+            data["proxy"] = "http://localhost:8080"
+            jsonFile.seek(0)  # rewind
+            json.dump(data, jsonFile)
+            jsonFile.truncate()
 
 
 def main():
